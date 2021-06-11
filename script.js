63a95b230b20e6557eef6920157b7411bc136b32
Kimberly Luke <kimberly_luke@cable.comcast.com>
2021-06-11 18:18:15 -0400

    Resolved

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
                   <li>Distance from Earth: ${json[3]["distance"]}</li>
                   <li>Number of Moons: ${json[3]["moons"]}</li>
                </ol>
-               <img src="${json[4]["image"]}">
+               <img src="${json[3]["image"]}">
                `
          });
       });
@@ -24,7 +24,7 @@
          event.preventDefault();
          let pilotName = document.querySelector("input[name=pilotName]");
          let copilotName = document.querySelector("input[name=copilotName]");
-         let fuelLevel = document.querySelector("input[name=fuelLevel]")
+         let fuelLevel = document.querySelector("input[name=fuelLevel]");
          let fuelValue = fuelLevel.value;
          let cargoMass = document.querySelector("input[name=cargoMass]");
          let cargoValue = cargoMass.value;
@@ -36,26 +36,27 @@
             pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch!`
             copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready for launch!`
             faultyItems.style.visibility = "visible";
+            console.log(cargoValue);
             if(fuelValue < 10000 && cargoValue > 10000) {
                fuelStatus.innerHTML = `Fuel Level ${fuelValue} is too low for launch.`;
                launchStatus.innerHTML = `Shuttle Not Ready for Launch!`;
                launchStatus.style.color = "red";
-               cargoMass.innerHTML = `${cargoValue} kg weighs too much for takeoff.`
+               cargoStatus.innerHTML = `${cargoValue} kg weighs too much for takeoff.`
             } else if (fuelValue < 10000) {
                fuelStatus.innerHTML = `Fuel Level ${fuelValue} is too low for launch.`;
                launchStatus.innerHTML = `Shuttle Not Ready for Launch!`;
                launchStatus.style.color = "red";
-               cargoValue.innerHTML = `Cargo mass low enough for launch.`
+               cargoStatus.innerHTML = `Cargo mass low enough for launch.`
             } else if (cargoValue > 10000) {
                launchStatus.innerHTML = `Shuttle not ready for launch.`;
                launchStatus.style.color = "red";
-               cargoMass.innerHTML = `${cargoValue} kg weighs too much for takeoff.`;
+               cargoStatus.innerHTML = `${cargoValue} kg weighs too much for takeoff.`;
                fuelStatus.innerHTML = `Fuel level high enough for launch.`;
             } else {
                launchStatus.innerHTML = `Shuttle is ready for launch.`;
-               launchStatus.style.color = `green`;
+               launchStatus.style.color = "green";
                fuelStatus.innerHTML = `Fuel level high enough for launch`;
-               cargoValue.innerHTML = `Cargo mass low enough for launch.`
+               cargoStatus.innerHTML = `Cargo mass low enough for launch.`
             };
          };
       });
